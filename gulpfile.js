28ef967d88b168cd136aa0a0bafd54e1d2af71c0
tkoleary <terrenceoleary@gmail.com>
2020-05-29 11:35:35 -0500

    changed 'languages' to 'script selectors'

--- gulpfile.js
+++ gulpfile.js
@@ -12,75 +12,75 @@ const browserSync       = require('browser-sync').create();
 sass.compiler           = require('dart-sass');
 
 var config = {
-    source:             './src/**/**.*',
-    sassFiles:          './src/sass/**/*.scss',
-    htmlFiles:          './src/**/*.html',
-    fontFolders:        './src/fonts/**/**.*',
-    autoprefixerOptions: {
-      browsers:         ['last 2 versions', '> 5%']
-    },
-    build:              './dist',
-    buildCss:           ['./dist/css'],
-    buildCssMin:        ['./dist/css/min'],
-    fontsFlat:          './dist/css/fonts/'
+	source:             './src/**/**.*',
+	sassFiles:          './src/sass/**/*.scss',
+	htmlFiles:          './src/**/*.html',
+	fontFolders:        './src/fonts/**/**.*',
+	autoprefixerOptions: {
+		browsers:         ['last 2 versions', '> 5%']
+	},
+	build:              './dist',
+	buildCss:           ['./dist/css'],
+	buildCssMin:        ['./dist/css/min'],
+	fontsFlat:          './dist/css/fonts/'
 };
 
 function compileSass(cb) {
-  return src(config.sassFiles)
+	return src(config.sassFiles)
 
-    // output non-minified CSS file
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-       outputStyle : 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer(config.autoprefixerOptions.browsers))
-    .pipe(dest(config.buildCss))
+	// output non-minified CSS file
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		outputStyle : 'expanded'
+	}).on('error', sass.logError))
+	.pipe(autoprefixer(config.autoprefixerOptions.browsers))
+	.pipe(dest(config.buildCss))
 
-    // output the minified version
-    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(dest(config.buildCssMin))
-  cb();
+	// output the minified version
+	.pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(sourcemaps.write())
+	.pipe(rename({ extname: '.min.css' }))
+	.pipe(dest(config.buildCssMin))
+	cb();
 }
 
 
 // Clean out the folder for a new build
 function wipe(cb) {
-  return src(config.build, {read: false})
-    .pipe(clean())
-  cb();
+	return src(config.build, {read: false})
+	.pipe(clean())
+	cb();
 }
 
 // Move the fonts from their subfolders to one big folder
 function fonts(cb) {
-  return src(config.fontFolders)
-    .pipe(flatten())
-    .pipe(dest(config.fontsFlat));
-  cb();
+	return src(config.fontFolders)
+	.pipe(flatten())
+	.pipe(dest(config.fontsFlat));
+	cb();
 }
 
 // Copy over HTML demo files
 function html(cb) {
-  return src(config.htmlFiles)
-    .pipe(dest(config.build));
-  cb();
+	return src(config.htmlFiles)
+	.pipe(dest(config.build));
+	cb();
 }
 
 // Serve locally
 function serve(cb) {
-  browserSync.init({
-    server: {
-      baseDir: './dist'
-    }
-  });
-  cb();
+	browserSync.init({
+		server: {
+			baseDir: './dist'
+		}
+	});
+	cb();
 }
 
 // Reload on change
 function refresh(cb) {
-  browserSync.reload()
-  cb();
+	browserSync.reload()
+	cb();
 }
 
 // Run 'gulp build' for all of the above
@@ -88,5 +88,5 @@ exports.build = series(wipe, fonts, compileSass, html, serve);
 
 // Watch and update on change
 exports.stream = function() {
-  watch(config.source,series(refresh));
+	watch(config.source,series(refresh));
 };
