f8c16c59dea15cac7bd55c475ae57d107c053423
tkoleary <terrenceoleary@gmail.com>
2020-07-26 15:03:05 -0500

    Added some configuration and content in docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	title: 'Lingua',
+	description: 'Internationalized Typography',
+	head: [
+		['link', { rel: 'icon', href: '/logo.png' }]
+	],
+	plugins: [
+	],
+	themeConfig: {
+		lastUpdated: false,
+		sidebar: [
+			["/", "Home"],
+			["/get-started", "Get started"],
+			["/languages", "Languages"]
+		],
+		nav: [
+			{ text: "Home", link: "/" }
+		],
+		displayAllHeaders: true
+	},
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-# Lingua Docs
+---
+home: true
+heroImage: false
+actionText: Get started →
+actionLink: /get-started
+
+footer: Copyright © Terrence Kevin Oleary - 2020
+meta:
+  - name: description
+    content: Internationalized Typography
+  - name: keywords
+    content: Lingua internationalization language script unicode font webfont
+---
